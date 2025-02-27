59f77ce7da7446c96bf3407fa64f5d834514ca25
bloodfallen <alexejberdnikovpuska@yandex.ru>
2025-02-27 18:56:10 +0700

    Hell Yeah

---
+++ containers/pair.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    pair<int, int> p = {1, 2};
+    cout << p.first << endl; // выводим первый член пары
+    cout << p.second << endl; // выводим второй член пары
+
+    
+
+    return 0;
+}
