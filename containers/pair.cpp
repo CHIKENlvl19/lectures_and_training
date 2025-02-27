31dec2d3cb14cc95a67782a99e8ef10b490a8db8
bloodfallen <alexejberdnikovpuska@yandex.ru>
2025-02-27 19:49:14 +0700

    Nailed pairs

--- containers/pair.cpp
+++ containers/pair.cpp
@@ -8,7 +8,14 @@ int main()
     cout << p.first << endl; // выводим первый член пары
     cout << p.second << endl; // выводим второй член пары
 
-    
+    p = make_pair(3, 4); // создаем пару с помощью make_pair
+    cout << p.first << endl;
+    cout << p.second << endl; 
+
+    pair<int, int> a, b;
+    a = {6, 7};
+    b = a;
+    cout << b.first << " " << b.second << endl;
 
     return 0;
 }
