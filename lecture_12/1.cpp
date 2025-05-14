e87b8845296e81d8e454d387fbb037dd22b30f6d
bloodfallen <74541116+CHIKENlvl19@users.noreply.github.com>
2025-05-15 01:00:12 +0700

    last lecture update

---
+++ lecture_12/1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Любимки, всех люблю, целую, обнимаю. Ваш папа." << endl;
+
+    // сегодня было без кода, соре
+
+    return 0;
+}
