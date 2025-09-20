585e168f72c364074ad05d8aad21f400fed8adb0
Alex <alexejberdnikovpuska@yandex.ru>
2025-09-20 16:07:56 +0700

    updated forwardlist

--- lecture_2/ForwardList.cpp
+++ lecture_2/ForwardList.cpp
@@ -5,17 +5,16 @@ using namespace std;
 struct Node
 {
     int key;
-    Node* next; // укащатель на следующий элемент
+    Node* next; // указатель на следующий элемент
 };
 
 struct ForwardList
 {
-    Node* head;
+    Node* head = nullptr;
 };
 
 void AddNode(Node* ptr, int key){
-    Node* newNode = new Node{key};
-    // newNode -> key = key;
+    Node* newNode = new Node{key, nullptr};
     newNode -> next = ptr -> next; // передаем указатель на следующий элемент для вставки
     ptr -> next = newNode; // создаем связь с новым элементом
 }
@@ -25,13 +24,55 @@ void createFL(ForwardList& fList, int keyBegin) {
 }
 
 void Delete(Node* ptr) {
+    if(!ptr || !ptr -> next)
+    {
+        return;
+    }
+
     Node* deleteNode = ptr -> next;
     ptr -> next = deleteNode -> next;
     
     delete deleteNode;
 }
 
+void print(ForwardList&  fList) {
+    Node* current = fList.head;
+
+    while(current != nullptr)
+    {
+        cout << current -> key << " " << current << " ";
+        current = current -> next;  
+    }
+
+    cout << "nullptr" << endl;
+}
+
+void clean(ForwardList& fList)  {
+    
+    while(fList.head)
+    {
+        Node* temp = fList.head;
+        fList.head = fList.head -> next;
+
+        delete temp;
+    }
+
+}
+
 int main(){
+    ForwardList list;
+    createFL(list, 15);
+
+    AddNode(list.head, 25);
+    AddNode(list.head, 35);
+
+    Delete(list.head);
+
+    print(list);
+
+    clean(list);
+
+    print(list);
 
     return 0;
 }
